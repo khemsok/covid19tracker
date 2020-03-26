79434751e2cbc3d9ee58802fdf974a723bbe1053
Khem Sok <khemsok97@gmail.com>
2020-03-26 18:51:55 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Paper from "@material-ui/core/Paper";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [allCases, setAllCases] = useState({});
+    useEffect(() => {
+        fetch("https://coronavirus-19-api.herokuapp.com/all")
+            .then(res => res.json())
+            .then(data => setAllCases(data));
+    }, []);
+
+    let display = null;
+    if (allCases.length === 0) {
+        display = <h1>Loading</h1>;
+    } else {
+        let cases = allCases["cases"];
+        let deaths = allCases["deaths"];
+        let recovered = allCases["recovered"];
+        display = (
+            <Paper
+                style={{ display: "inline-block", padding: "20px" }}
+                elevation={3}
+            >
+                <ul
+                    style={{
+                        listStyleType: "none",
+                        padding: "0",
+                        display: "inline-block"
+                    }}
+                >
+                    <li>😷 Cases: {cases}</li>
+                    <li>💀 Deaths: {deaths}</li>
+                    <li>😊 Recovered: {recovered}</li>
+                </ul>
+            </Paper>
+        );
+    }
+
+    return (
+        <div className="App">
+            <h1>Covid-19 🦠</h1>
+            <h3>Global Overview</h3>
+            {display}
+        </div>
+    );
 }
 
 export default App;
